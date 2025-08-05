8937fdb6d1902b4210461d1ad9318de5e6b1ff01
Anju Vijay <anjuvijaytk@gmail.com>
2025-08-05 17:07:07 +0400

    React,tailwind,react-dom was installed. Home page was designed. onSubmitHandler functionality was added.

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import {useNavigate} from 'react-router-dom'
+import { useState } from 'react'
+
+const Home = () => {
+    const navigate=useNavigate()
+    const [repoInput,setRepoInput]=useState('')
+    const onSubmitHandler=()=>{
+        const[owner, repo]=repoInput.replace('https://github.com/', '').split('/')
+        if(!owner || !repo){
+            alert("Please enter valid repo/owner")
+        }
+        navigate(`/list-repo-files/${owner}/${repo}`)
+    }
+  return (
+    <div className='w-full flex justify-center items-center h-screen'>
+        <div className='bg-lime-300 opacity-60 rounded p-4 w-3/4 h-1/4 flex justify-between items-center '>
+            <input value={repoInput} onChange={(e)=>setRepoInput(e.target.value)} className='bg-lime-200 font-semibold w-2/3 h-14 p-2 rounded border border-lime-400' type="text" placeholder='Enter the git repo' />
+            <button className='p-3 bg-lime-200 h-14 rounded cursor-pointer font-semibold border border-lime-400 shadow-2xl hover:bg-lime-400'>Get Repo Files</button>
+        </div>
+    </div>
+  )
+}
+
+export default Home
